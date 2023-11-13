3aff34bbbb644ef530802ac1ad2339e893fa3dc5
Randall Winkhart <idgr@tutanota.com>
2023-11-13 01:23:14 -0500

    Initial support for mapping buttons from environment variables

--- gpbuttond.go
+++ gpbuttond.go
@@ -1,21 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bendahl/uinput"
 	"github.com/warthog618/gpiod"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-// map GPIO pins to keys - auxiliary pins do not yet have programmed functions
-var pinUp = 19
-var pinDown = 6
-var pinLeft = 26
-var pinRight = 5
-
-//var pinAux1 = 16
-//var pinAux2 = 20
-//var pinAux3 = 21
-//var pinAux4 = 13
+// pin to key mapping, global
+var pinMap [10][2]int
 
 // create channel for signaling end of routineHold
 var exitChannel = make(chan bool)
@@ -24,32 +20,20 @@ var exitChannel = make(chan bool)
 var kbd, _ = uinput.CreateKeyboard("/dev/uinput", []byte("ttypodvirtualkbd"))
 
 // meant to run as a Go routine (launched from eventHandler) - repeats keystroke until GPIO button is no longer held down
-func routineHold(offset int, inputChannel <-chan bool) {
+func routineHold(keycode int, inputChannel <-chan bool) {
 	for {
 		select {
 		case <-inputChannel:
 			return
 		default:
-			switch offset {
-			case pinUp:
-				kbd.KeyPress(uinput.KeyUp)
-				time.Sleep(150 * time.Millisecond)
-			case pinDown:
-				kbd.KeyPress(uinput.KeyDown)
-				time.Sleep(150 * time.Millisecond)
-			case pinLeft:
-				kbd.KeyPress(uinput.KeyLeft)
-				time.Sleep(150 * time.Millisecond)
-			case pinRight:
-				kbd.KeyPress(uinput.KeyEnter)
-				time.Sleep(150 * time.Millisecond)
-			}
+			kbd.KeyPress(keycode)
+			time.Sleep(150 * time.Millisecond)
 		}
 	}
 }
 
 // called whenever a GPIO event is detected on a watched line - uses routineHold as a Go routine to repeat keystrokes for as long as buttons are held down
-func eventHandler(offset int) func(_ gpiod.LineEvent) {
+func eventHandler(keycode int) func(_ gpiod.LineEvent) {
 	return func(evt gpiod.LineEvent) {
 		if evt.Type == gpiod.LineEventRisingEdge { // button release
 			// send signal to exit Go routine (stops repeating keystrokes)
@@ -57,7 +41,7 @@ func eventHandler(offset int) func(_ gpiod.LineEvent) {
 
 		} else if evt.Type == gpiod.LineEventFallingEdge { // button press
 			// launch Go routine to repeat keystrokes until a LineEventRisingEdge event is received
-			go routineHold(offset, exitChannel)
+			go routineHold(keycode, exitChannel)
 		}
 	}
 }
@@ -66,15 +50,68 @@ func main() {
 	// ensure closure of keyboard after program exits
 	defer kbd.Close()
 
+	// check environment variables (settings)
+	// pin to key mapping - local
+
+	var mapEnv, mapPresent = os.LookupEnv("GPBD_MAP")
+	var mapEnvSplit []string
+	var pairCount int
+	if mapPresent {
+		mapEnvSplit = strings.Split(mapEnv, ",")
+		pairCount = len(mapEnvSplit)
+	} else {
+		fmt.Println("Error: no pairings provided - see \"gpbuttond help\"\n\nNOTE THAT \"gpbuttond help\" FUNCTIONALITY HAS NOT YET BEEN ADDED AND YOU ARE USING AN UNTAGGED RELEASE")
+		os.Exit(1)
+	}
+
+	for i, pair := range mapEnvSplit {
+		var intConvert [2]int
+		for i, num := range strings.Split(pair, ":") {
+			intConvert[i], _ = strconv.Atoi(num)
+		}
+		pinMap[i] = [2]int{intConvert[0], intConvert[1]}
+	}
+
 	// begin edge detection - will call eventHandler whenever a watched GPIO line changes state
-	lineUp, _ := gpiod.RequestLine("gpiochip0", pinUp, gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinUp)))
-	defer lineUp.Close()
-	lineDown, _ := gpiod.RequestLine("gpiochip0", pinDown, gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinDown)))
-	defer lineDown.Close()
-	lineLeft, _ := gpiod.RequestLine("gpiochip0", pinLeft, gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinLeft)))
-	defer lineLeft.Close()
-	lineRight, _ := gpiod.RequestLine("gpiochip0", pinRight, gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinRight)))
-	defer lineRight.Close()
+	// TODO EDIT HERE TO ADD MORE BUTTONS (1/2)
+	const buttonCount = 10
+	// TODO END EDIT ZONE (1/2)
+	for i := 0; i < min(pairCount, buttonCount); i++ {
+		switch i + 1 {
+		case 1:
+			line1, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line1.Close()
+		case 2:
+			line2, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line2.Close()
+		case 3:
+			line3, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line3.Close()
+		case 4:
+			line4, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line4.Close()
+		case 5:
+			line5, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line5.Close()
+		case 6:
+			line6, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line6.Close()
+		case 7:
+			line7, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line7.Close()
+		case 8:
+			line8, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line8.Close()
+		case 9:
+			line9, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line9.Close()
+		case 10:
+			line10, _ := gpiod.RequestLine("gpiochip0", pinMap[i][0], gpiod.WithPullUp, gpiod.WithBothEdges, gpiod.WithDebounce(20*time.Millisecond), gpiod.WithEventHandler(eventHandler(pinMap[i][1])))
+			defer line10.Close()
+			// TODO EDIT HERE TO ADD MORE BUTTONS (2/2)
+			// TODO END EDIT ZONE (2/2)
+		}
+	}
 
 	// run FOREVER >:)
 	for {
