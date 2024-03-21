903ed9b5e341aee9e9291d69c53e6af6e653aa32
Randall Winkhart <idgr@tutanota.com>
2024-03-21 16:21:38 -0400

    Update support scripts for main.go

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 gofmt -l -w -s .
-git add -f .gitignore 15gpbuttond.start APKBUILD build-all-targets.sh commit.sh go.mod go.sum gpbuttond.go LICENSE README.md
+git add -f .gitignore 15gpbuttond.start APKBUILD build-all-targets.sh commit.sh go.mod go.sum LICENSE main.go README.md
 git commit -m "$1"
 git push
