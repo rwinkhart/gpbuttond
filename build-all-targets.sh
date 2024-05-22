c5fbc46d6060000db1d133af3081cec770896382
Randall Winkhart <idgr@tutanota.com>
2024-05-22 15:16:31 -0400

    Strip debug information from binaries, specify CGO_ENABLED=0

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 rm -rf ./output
 mkdir -p ./output
-go mod tidy
-GOOS=linux GOARCH=arm GOARM=6 go build -o output/gpbuttond-armv6
-GOOS=linux GOARCH=arm GOARM=7 go build -o output/gpbuttond-armv7
-GOOS=linux GOARCH=arm64 go build -o output/gpbuttond-armv8-aarch64
-GOOS=linux GOARCH=riscv64 go build -o output/gpbuttond-riscv64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build --ldflags="-s -w" -o output/gpbuttond-armv6
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build --ldflags="-s -w" -o output/gpbuttond-armv7
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build --ldflags="-s -w" -o output/gpbuttond-armv8-aarch64
+CGO_ENABLED=0 GOOS=linux GOARCH=riscv64 go build --ldflags="-s -w" -o output/gpbuttond-riscv64
