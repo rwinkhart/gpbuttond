5e67be2eed5a3a87b0b5c729538a6d9431082c3f
Randall Winkhart <idgr@tutanota.com>
2024-08-05 15:18:38 -0400

    Update build/commit tooling to use more appropriate format

--- build-all-targets.sh
+++ build-all-targets.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf ./output
 mkdir -p ./output
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build --ldflags="-s -w" -o output/gpbuttond-armv6
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build --ldflags="-s -w" -o output/gpbuttond-armv7
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build --ldflags="-s -w" -o output/gpbuttond-armv8-aarch64
-CGO_ENABLED=0 GOOS=linux GOARCH=riscv64 go build --ldflags="-s -w" -o output/gpbuttond-riscv64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags="-s -w" -trimpath -o output/gpbuttond-armv6h
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-s -w" -trimpath -o output/gpbuttond-armv7h
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -trimpath -o output/gpbuttond-armv8-aarch64
+CGO_ENABLED=0 GOOS=linux GOARCH=riscv64 go build -ldflags="-s -w" -trimpath -o output/gpbuttond-riscv64
--- commit.sh
+++ commit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 gofmt -l -w -s .
-git add -f .gitignore 15gpbuttond.start APKBUILD build-all-targets.sh commit.sh go.mod go.sum LICENSE main.go README.md
-git commit -m "$1"
+git commit -am "$1"
 git push
